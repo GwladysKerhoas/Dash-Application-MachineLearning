71892aeeba3b3c0e239564ae9647188ec5b67fcd
GwladysKerhoas <73121667+GwladysKerhoas@users.noreply.github.com>
2021-12-05 15:09:00 +0100

    Add files via upload

--- regression.py
+++ regression.py
@@ -16,41 +16,43 @@ from sklearn.svm import SVR
 from sklearn.tree import DecisionTreeRegressor
 
 
+# Regression class to gather all the classification algorithm
 class regression:
 
+    # Linear regression algorithm
     def linear_reg(target,predictor):
         
         X = pd.DataFrame(predictor)
-        #normalisation des variables explicatives quantitatives
+        
+        # Normalization of quantitative explanatory variables
         transformer = Normalizer().fit(X)
         transformer
         transformer.transform(X)
-        #récupération variable cible
+        
+        # Target variable
         y = pd.DataFrame(target)
         
-        #split train-test
+        # Split train-test
         X_train, X_test, y_train, y_test = train_test_split(X, y.values.ravel(), train_size=0.7, random_state=10)
         
-        scorer = make_scorer(r2_score) #Type de score
-        regressionLineaire = LinearRegression() #Instanciation du modèle
-        params = {'fit_intercept':[True, False], 'normalize':[True, False]} #Paramètres à tester
+        scorer = make_scorer(r2_score) #Type of score
+        regressionLineaire = LinearRegression() # Model instanciation
+        params = {'fit_intercept':[True, False], 'normalize':[True, False]} # Paramaters to test
         clf = GridSearchCV(regressionLineaire, param_grid=params, cv=5,scoring=scorer, n_jobs=5)
-        clf.fit(X_train, y_train) #On applique sur les données d'apprentissage
+        clf.fit(X_train, y_train) # We apply on the training data
         
-        best_param = clf.best_params_ #Meilleur paramètrage
-        best_score = round(clf.best_score_,2) #Meilleur score
+        best_param = clf.best_params_ # Better setting
+        best_score = round(clf.best_score_,2) # Best score
         
-        #Instanciation du modèle avec paramètres optimaux identifiés
+        # Instantiation of the model with identified optimal parameters
         regressionLineaire = LinearRegression(fit_intercept=best_param["fit_intercept"], normalize=best_param["normalize"]) 
 
         start = time()
         
-        #prédictions
+        # Predictions
         y_pred = cross_val_predict(regressionLineaire, X_test, y_test, cv=5)
         r2 = round(r2_score(y_test, y_pred),2)
         mse = round(mean_squared_error(y_test, y_pred),2)
-        
-        #metrics_class = metrics.classification_report(y_test, y_pred)
         
         # Best parameter
         labels_values = []
@@ -70,7 +72,7 @@ class regression:
         end = time()
         time_execution = round(end-start,2)
         
-        #scatter plot
+        # Scatter plot
         fig = go.Figure()
         absi = list(range(len(y_test)))
         
@@ -79,37 +81,40 @@ class regression:
 
         return best_param, best_score, r2, mse, time_execution, fig, fig_table
     
-    
+    # Support Vector Regression algorithm
     def svr(target,predictor):
         X = pd.DataFrame(predictor)
-        #normalisation des variables explicatives quantitatives
+        
+        # Normalization of quantitative explanatory variables
         transformer = Normalizer().fit(X)
         transformer
         transformer.transform(X)
-        #récupération variable cible
+        
+        # Target variable
         y = pd.DataFrame(target)
         
-        #split train-test
+        # Split train-test
         X_train, X_test, y_train, y_test = train_test_split(X, y.values.ravel(), train_size=0.7, random_state=10)
         
-        scorer = make_scorer(r2_score) #Type de score
-        modelsvr = SVR() #Instanciation du modèle
+        scorer = make_scorer(r2_score) #Type of score
+        modelsvr = SVR() # Instanciation du modèle
         params = {'kernel':['linear', 'poly', 'rbf', 'sigmoid'], 
                   'gamma':['scale', 'auto'],
                   'C' : [1,5,10],
                   'degree' : [3,8],
-                  'coef0' : [0.01,10,0.5]} #Paramètres à tester
+                  'coef0' : [0.01,10,0.5]} # Paramaters to test
         clf = GridSearchCV(modelsvr, param_grid=params, cv=5,scoring=scorer, n_jobs=5)
-        clf.fit(X_train, y_train) #On applique sur les données d'apprentissage
+        clf.fit(X_train, y_train) # We apply on the training data
                 
-        best_param = clf.best_params_ #Meilleur paramètrage
-        best_score = round(clf.best_score_,2) #Meilleur score
+        best_param = clf.best_params_ # Better setting
+        best_score = round(clf.best_score_,2) # Best score
         
-        #Instaciation du modèle avec paramètres optimaux identifiés
+        # Instantiation of the model with identified optimal parameters
         modelsvr = SVR(C=best_param["C"], coef0=best_param["coef0"], degree=best_param["degree"], gamma=best_param["gamma"], kernel=best_param["kernel"]) 
      
         start = time()
         
+        # Predictions
         y_pred = cross_val_predict(modelsvr, X_test, y_test, cv=5)
         r2 = round(r2_score(y_test, y_pred),2)
         mse = round(mean_squared_error(y_test, y_pred),2)
@@ -132,7 +137,7 @@ class regression:
         end = time()
         time_execution = round(end-start,2)
         
-        #scatter plot
+        # Scatter plot
         fig = go.Figure()
         absi = list(range(len(y_test)))
         
@@ -141,34 +146,37 @@ class regression:
 
         return best_param, best_score, r2, mse, time_execution, fig, fig_table
     
-    
+    # Decision tree regression algorithm
     def dec_tree_reg(target,predictor):
         X = pd.DataFrame(predictor)
-        #normalisation des variables explicatives quantitatives
+        
+        # Normalization of quantitative explanatory variables
         transformer = Normalizer().fit(X)
         transformer
         transformer.transform(X)
-        #récupération variable cible
+        
+        # Target variable
         y = pd.DataFrame(target)
         
-        #split train-test
+        # Split train-test
         X_train, X_test, y_train, y_test = train_test_split(X, y.values.ravel(), train_size=0.7, random_state=10)
         
-        scorer = make_scorer(r2_score) #Type de score
-        treeReg = DecisionTreeRegressor() #Intaciation du modèle
+        scorer = make_scorer(r2_score) # Type of score
+        treeReg = DecisionTreeRegressor() # Model instanciation
         params = {'splitter':['best', 'random'],
-                  'max_features':['auto', 'sqrt','log2']} #Paramètres à tester
+                  'max_features':['auto', 'sqrt','log2']} # Paramaters to test
         clf = GridSearchCV(treeReg, param_grid=params, cv=5,scoring=scorer, n_jobs=5)
-        clf.fit(X_train, y_train) #On applique sur les données d'apprentissage
+        clf.fit(X_train, y_train) # We apply on the training data
                         
-        best_param = clf.best_params_ #Meilleur paramètrage
-        best_score = round(clf.best_score_,2) #Meilleur score
+        best_param = clf.best_params_ # Better setting
+        best_score = round(clf.best_score_,2) # Best score
         
-        #Instaciation du modèle avec paramètres optimaux identifiés
+        # Instantiation of the model with identified optimal parameters
         treeReg = DecisionTreeRegressor(max_features=best_param["max_features"], splitter=best_param["splitter"])
 
         start = time()
         
+        # Predictions
         y_pred = cross_val_predict(treeReg, X_test, y_test, cv=5)
         r2 = round(r2_score(y_test, y_pred),2)
         mse = round(mean_squared_error(y_test, y_pred),2)
@@ -191,7 +199,7 @@ class regression:
         end = time()
         time_execution = round(end-start,2)
         
-        #scatter plot
+        # Scatter plot
         fig = go.Figure()
         absi = list(range(len(y_test)))
         
